b1b167a962b6a3e5d687e58abc0d2b7e9ba18bb0
dukegreed <75464788+dukegreed@users.noreply.github.com>
2023-02-04 00:53:32 -0800

    .

---
+++ data.py
@@ -0,0 +1,165 @@
+data = [
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/SwZW2Z3.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Male',
+        'link': 'https://i.imgur.com/0kVWI1W.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Other',
+        'link': 'https://i.imgur.com/o14yHsE.jpg',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/ZiVBdYz.jpg',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Male',
+        'link': 'https://i.imgur.com/2l3ps9D.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Turtle',
+        'link': 'https://i.imgur.com/5h6CCzV.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Male',
+        'link': 'https://i.imgur.com/9Z9GiwW.jpg',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/aZjiNtO.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Tree',
+        'link': 'https://i.imgur.com/itIrLtW.png',
+        'ai' : '1'
+    },
+
+    {
+        'name': 'Photo',
+        'link': 'https://i.imgur.com/FgxipBZ.png',
+        'ai' : '1'
+    },
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/Rc4sizX.jpg',
+        'ai' : '0'
+    },
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/daPkfdj.jpg',
+        'ai' : '0'
+    },
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/aYgfYmZ.jpg',
+        'ai' : '0'
+    },
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/FcKc7wB.jpg',
+        'ai' : '0'
+    },
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/YpGK3nE.jpg',
+        'ai' : '0'
+    },
+    {
+        'name': 'Male',
+        'link': 'https://i.imgur.com/w9JHWsR.jpg',
+        'ai' : '0'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/iy0g0XA.jpg',
+        'ai' : '0'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/mG9RTGk.jpg',
+        'ai' : '0'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/encZfqY.jpg',
+        'ai' : '0'
+    },
+
+    {
+        'name': 'Female',
+        'link': 'https://i.imgur.com/GSxedVm.jpg',
+        'ai' : '0'
+    }
+]
+# Template
+# ,
+
+#     {
+#         'name': '',
+#         'link': '',
+#         'ai' : '0'
+#     }
+
+
+
+data_sources = [
+    
+    {
+        'name': 'u/3deal',
+        'link': 'https://i.imgur.com/2l3ps9D.png'
+    },
+    {
+        'name': 'u/PartialParsley',
+        'link': 'https://i.imgur.com/5h6CCzV.png'
+    },
+    {
+        'name': 'u/pulp_hero',
+        'link': 'https://i.imgur.com/9Z9GiwW.jpg'
+    },
+    {
+        'name': 'u/RamenWithOJ',
+        'link': 'https://i.imgur.com/aZjiNtO.png'
+    },
+    {
+        'name': 'u/lman777',
+        'link': 'https://i.imgur.com/itIrLtW.png'
+    },
+    {
+        'name': 'u/Seoinetru',
+        'link': 'https://i.imgur.com/FgxipBZ.png'
+    },
+    {
+        'name': 'u/BenMoranArtist',
+        'link': ('https://i.imgur.com/Rc4sizX.jpg', 
+        'https://i.imgur.com/daPkfdj.jpg','https://i.imgur.com/aYgfYmZ.jpg',
+        'https://i.imgur.com/FcKc7wB.jpg','https://i.imgur.com/YpGK3nE.jpg',
+        'https://i.imgur.com/w9JHWsR.jpg')
+    },
+    {
+        'name': 'GAN',
+        'link': ('https://i.imgur.com/iy0g0XA.jpg','https://i.imgur.com/mG9RTGk.jpg',
+        'https://i.imgur.com/encZfqY.jpg','https://i.imgur.com/GSxedVm.jpg')
+    }
+]
